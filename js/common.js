aafabf4f5e4a2eab8415d57ef61d7d70ca4f7945
AnnaKuhir <58226384+AnnaKuhir@users.noreply.github.com>
2020-04-28 12:27:48 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,90 @@
+
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
+});
+
+// $(document).ready(function(){
+//   $('.services__slider').slick({
+//     arrows: false,
+//     dots: true,
+//     slidesToShow: 1.5,
+//   });
+// })
+
+$(window).on("load resize", function () {
+    $(".services__slider").each(function () {
+        var $slider = $(this);
+        if ($(window).width() > 768) {
+            if ($slider.hasClass("slick-initialized")) {
+                $slider.slick("unslick");
+            }
+        } else {
+            if (!$slider.hasClass("slick-initialized")) {
+                $slider.slick({
+                    initialSlide: 0,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    speed: 1000,
+                    easing: 'easeInOutQuart',
+                    arrows: false,
+                    dots: true,
+                    infinite: false,
+                    responsive: [
+                        {
+                            breakpoint: 600,
+                            settings: {
+                                slidesToShow: 1.2,
+                            }
+                        },
+                    ]
+                });
+            }
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $('.coaches__slider').slick({
+        slidesToShow: 2,
+        infinite: false,
+        prevArrow: $('.arrow__prev'),
+        nextArrow: $('.arrow__next'),
+
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1.7
+                }
+
+            },
+            {
+                breakpoint: 857,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                    slidesToShow: 1.5
+                }
+
+            },
+            {
+                breakpoint: 595,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                    slidesToShow: 1.2
+                }
+
+            }
+        ]
+
+    }).on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $('.arrow__info').text(i + ' ' + 'of' + ' ' + slick.slideCount);
+    });
+})
+
